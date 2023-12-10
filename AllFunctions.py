799291bf4c5a9be247b4ce3613961494942c024f
venkat <venks88@gmail.com>
2023-12-09 21:04:38 -0500

    Updated folder name

--- AllFunctions.py
+++ AllFunctions.py
@@ -1,15 +1,14 @@
-def get_opn(operator):
+def get_opn(operator, a,b):
     if operator == "+":
-        def sumn(a,b):
+        def sumn():
             return a+b;
         return sumn
     elif operator == "-":
-        def sums(a,b):
+        def sums():
             return a-b;
         return sums
     else:
         print("Invalid Operator")
 
 
-add_func = get_opn("+");
-result = sumn(20, 30);
+add_func = get_opn("+", 20, 30);
